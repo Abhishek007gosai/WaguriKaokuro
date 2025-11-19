1037b96f715bf9657a6952df7f82d00e5dcd1d76
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-19 22:02:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,27 +12,27 @@ id_pattern = re.compile(r'^.\d+$')
 # --
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
+    API_ID    = os.environ.get("API_ID", "23537462")
+    API_HASH  = os.environ.get("API_HASH", "c9599a5aa61ee8ca4f5e778d20c61f24")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     PORT = os.environ.get("PORT", "8980")
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "RexBots")     
+    DB_NAME = os.environ.get("DB_NAME", "cluster0")     
     DB_URL  = os.environ.get("DB_URL", "")
  
     # other configs
-    ADMIN_URL = "https://t.me/adityaabhinav"
+    ADMIN_URL = "https://t.me/EternalsHelplineBot"
     DUMP_CHANNEL = os.environ.get("DUMP_CHANNEL", "-1002257657458")
     DUMP = True
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://ibb.co/Cs0zsrCn")
-    LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://ibb.co/RGx4RmDg")
+    START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
+    LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
     OWNER_ID = int(os.environ.get("OWNER_ID", "6701907262"))
-    SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002871570975"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    FSUB_PIC = os.environ.get("FSUB_PIC", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1001607131442"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
+    FSUB_PIC = os.environ.get("FSUB_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "YoruXRobot")
     LEADERBOARD_DELETE_TIMER = 30
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
