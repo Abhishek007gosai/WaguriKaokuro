048fe758866861e01f92ba829e70d206dd9a4734
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-26 02:36:04 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -113,11 +113,12 @@ async def cb_handler(client, query: CallbackQuery):
         elif data == "file_names":
             format_template = await rexbots.get_format_template(user_id)
             await query.message.edit_text(
-                text=Config.FILE_NAME_TXT.format(format_template=format_template),
+                text=Config.FILE_NAME_TXT.format(**format_template),
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"), InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
-                ])
+                    [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
+                     InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
+                 ])
             )    
         elif data == "thumbnail":
             await query.message.edit_text(
