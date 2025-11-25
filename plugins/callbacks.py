e348d90e9f436f8e45268be8ed61b93640160c00
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-26 03:07:44 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -95,23 +95,19 @@ async def cb_handler(client, query: CallbackQuery):
                     InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close"),
                     InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="help")
                 ]])
+            )            
+        elif data == "file_names":
+            format_template = await rexbots.get_format_template(user_id)
+            await query.message.edit_text(
+                text=Config.FILE_NAME_TXT.format(format_template=format_template),
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([
+                    [
+                        InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
+                        InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
+                    ]
+                ])
             )
-            
-elif data == "file_names":
-    format_template = await rexbots.get_format_template(user_id)
-    await query.message.edit_text(
-        text=Config.FILE_NAME_TXT.format(format_template=format_template),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
-                InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
-            ]
-        ])
-    )
-
-
-
         elif data == "donate":
             await query.message.edit_text(
                 text=Config.DONATE_TXT,
