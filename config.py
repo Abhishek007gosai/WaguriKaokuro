c61c5ae60035018b97c01e055a58e97ab57d700d
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-26 01:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class Config(object):
     SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002380726940"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
     FSUB_PIC = os.environ.get("FSUB_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "YoruXRobot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Auto_Rename_XRoBot")
     LEADERBOARD_DELETE_TIMER = 30
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
