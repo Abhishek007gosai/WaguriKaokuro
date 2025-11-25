cc4a5f644ad2a2065925af670101368585863372
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-26 02:52:00 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -103,8 +103,8 @@ async def cb_handler(client, query: CallbackQuery):
                disable_web_page_preview=True,
                reply_markup=InlineKeyboardMarkup([
                    [
-                      InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
-                      InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
+                       InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
+                       InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
                    ]
               ])
         )
@@ -116,16 +116,19 @@ async def cb_handler(client, query: CallbackQuery):
                     [InlineKeyboardButton("• ʙᴀᴄᴋ", callback_data="help"), InlineKeyboardButton("ᴏᴡɴᴇʀ •", url='https://t.me/EternalsHelplineBot')]
                 ])
             )
-        elif data == "file_names":
-            format_template = await rexbots.get_format_template(user_id)
-            await query.message.edit_text(
-                text=Config.FILE_NAME_TXT.format(**format_template),
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
-                     InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")]
-                 ])
-            )    
+       elif data == "file_names":
+           format_template = await rexbots.get_format_template(user_id)
+
+           await query.message.edit_text(
+               text=Config.FILE_NAME_TXT.format(format_template=format_template),
+               disable_web_page_preview=True,
+               reply_markup=InlineKeyboardMarkup([
+            [
+                        InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="close"),
+                InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
+            ]
+        ])
+    )
         elif data == "thumbnail":
             await query.message.edit_text(
                 text=Config.THUMBNAIL_TXT,
