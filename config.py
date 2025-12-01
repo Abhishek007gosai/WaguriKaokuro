ffb1b2737a8fc44225dcccbd18690d70e4c63794
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-12-02 02:02:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,11 +27,11 @@ class Config(object):
     DUMP = False
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
-    LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
+    LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://files.catbox.moe/8iu8dv.jpg")
     OWNER_ID = int(os.environ.get("OWNER_ID", "7654385403"))
     SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002380726940"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
-    FSUB_PIC = os.environ.get("FSUB_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
+    FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/8iu8dv.jpg")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Auto_Rename_XRoBot")
     LEADERBOARD_DELETE_TIMER = 30
     # wes response configuration     
