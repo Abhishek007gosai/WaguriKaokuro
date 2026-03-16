569aeb5b73e3c27344367d49ce68465ef84bce13
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-03-17 03:49:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "23537462")
     API_HASH  = os.environ.get("API_HASH", "c9599a5aa61ee8ca4f5e778d20c61f24")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8734890736:AAF2GOiSuNr6NpF5tb131CdOxX0k6M6_D58")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     PORT = os.environ.get("PORT", "8980")
 
     # database config
     DB_NAME = os.environ.get("DB_NAME", "cluster0")     
-    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://autorename:fbMbsVFICHccoTup@cluster0.uygy8qe.mongodb.net/")
+    DB_URL  = os.environ.get("DB_URL", "")
  
     # other configs
     ADMIN_URL = "https://t.me/Honkai_Bots"
