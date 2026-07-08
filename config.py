b6f9be85b96acea3cbb714e12378a91d577fd57f
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-07-09 01:37:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,13 +22,13 @@ class Config(object):
     DB_URL  = os.environ.get("DB_URL", "")
  
     # other configs
-    ADMIN_URL = "https://t.me/Honkai_Bots"
+    ADMIN_URL = "https://t.me/EternalsHelplineBot"
     DUMP_CHANNEL = -1002734211536
     DUMP = False
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/kgSv5sKP/3c10c3a8fc8d.jpg")
     LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://files.catbox.moe/8iu8dv.jpg")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "8667251104"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "8771195193"))
     SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002380726940"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
     FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/0c2o1j.jpg")
