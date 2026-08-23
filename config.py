f85d8f248d80291a8790a8d71dca4c8a82d13772
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-08-23 16:44:13 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -54,9 +54,9 @@ class Config(object):
 ➲ {season} / Season - ᴛᴏ ʀᴇᴘʟᴀᴄᴇ ꜱᴇᴀꜱᴏɴ ɴᴜᴍʙᴇʀ
 ➲ {quality} / Quality - ᴛᴏ ʀᴇᴘʟᴀᴄᴇ ǫᴜᴀʟɪᴛʏ
 ➲ {audio} / Audio - ᴛᴏ ʀᴇᴘʟᴀᴄᴇ ᴀᴜᴅɪᴏ ɪɴꜰᴏ
-➲ {name} - ꜰɪʀꜱᴛ 3 ʟᴇᴛᴛᴇʀꜱ ᴏꜰ ᴏʀɪɢɪɴᴀʟ ꜰɪʟᴇ ɴᴀᴍᴇ
+➲ {name} - ꜰɪʀꜱᴛ 3 ᴡᴏʀᴅꜱ ᴏꜰ ᴀɴɪᴍᴇ / ꜱʜᴏᴡ ᴛɪᴛʟᴇ ꜰʀᴏᴍ ꜰɪʟᴇ ɴᴀᴍᴇ
 
-<b>‣ ꜰᴏʀ ᴇx:- </b>`/autorename [S{season} E{episode}] {name} [Quality] [Audio]`
+<b>‣ ꜰᴏʀ ᴇx:- </b>`/autorename [S{season} E{episode}] {name} Your Anime Name [Quality] [Audio] @Honkai_Bots`
 
 Don't use .mkv/.mp4, Bot Will Detect The Format Automatically
 
@@ -98,7 +98,8 @@ Don't use .mkv/.mp4, Bot Will Detect The Format Automatically
 <b><i>💞  ɪꜰ ʏᴏᴜ ʟɪᴋᴇ ᴏᴜʀ ʙᴏᴛ ꜰᴇᴇʟ ꜰʀᴇᴇ ᴛᴏ ᴅᴏɴᴀᴛᴇ ᴀɴʏ ᴀᴍᴏᴜɴᴛ ₹𝟷𝟶, ₹𝟸𝟶, ₹𝟻𝟶, ₹𝟷𝟶𝟶, ᴇᴛᴄ.</i></b>
 
 ᴅᴏɴᴀᴛɪᴏɴs ᴀʀᴇ ʀᴇᴀʟʟʏ ᴀᴘᴘʀᴇᴄɪᴀᴛᴇᴅ ɪᴛ ʜᴇʟᴘs ɪɴ ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ
-ɪғ ʏᴏᴜ ᴡɪsʜ ʏᴏᴜ ᴄᴀɴ sᴇɴᴅ ᴜs ss"""
+ɪғ ʏᴏᴜ ᴡɪsʜ ʏᴏᴜ ᴄᴀɴ sᴇɴᴅ ᴜs ss
+ᴏɴ - @Honkai_Bots"""
 
     HELP_TXT = """<b>ʜᴇʀᴇ ɪꜱ ʜᴇʟᴘ ᴍᴇɴᴜ ɪᴍᴘᴏʀᴛᴀɴᴛ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
